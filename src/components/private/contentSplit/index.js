3791700ecd2a594bc47b190fb8f93e9ae12ea38e
wululing <wululing@joyy.com>
2022-04-16 03:49:50 +0800

    fix: ContentSplit 去除判断

--- src/components/private/contentSplit/index.js
+++ src/components/private/contentSplit/index.js
@@ -1,13 +1,13 @@
 import ContentSplit from './ContentSplit'
-import install from "vuex/dist/vuex.mjs";
+// import install from "vuex/dist/vuex.mjs";
 const components={
 	install(Vue){
 		Vue.component('ContentSplit',ContentSplit)
   }
 }
 //判断
-if(typeof window !=='undefined' && window.Vue){
-	install(window.Vue);
-}
+// if(typeof window !=='undefined' && window.Vue){
+// 	install(window.Vue);
+// }
  
 export default components;
